5b1377be62caf384b0c8959e5a49833f0851d7e4
sculpt0r <github@sculpt0r.net>
2021-12-16 22:22:27 +0100

    Add runner config reflected from mocha

--- packages/ava-runner/src/index.ts
+++ packages/ava-runner/src/index.ts
@@ -1,6 +1,6 @@
 import { declareFactoryPlugin, PluginKind } from '@stryker-mutator/api/plugin';
 
-import strykerValidationSchema from '../schema/mocha-runner-options.json';
+import strykerValidationSchema from '../schema/ava-runner-options.json';
 
 import { createAvaTestRunner, createAvaTestRunnerFactory } from './ava-runner-factories';
 import { AvaTestRunner } from './ava-test-runner';
